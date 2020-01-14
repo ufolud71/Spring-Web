e8d6d79fd0c700ee8d10b2927c7a8ed636161d42
ufolud71 <chudy.orfeusz@gmail.com>
2020-01-14 20:42:04 +0100

    Shell scritp

--- showtasks.bat
+++ showtasks.bat
@@ -1,17 +1,14 @@
-./runcrud.bat
-
-:openbrowser
-start chrome
-if "%ERRORLEVEL%" == "0" goto openurl
-echo.
-echo Could not open browser.
-goto fail
 
 :openurl
 start chrome "http://localhost:8080/crud/v1/task/getTasks"
-if "%ERRORLEVEL%" == "0" goto end
+if "%ERRORLEVEL%" == "0" goto runcrud
 echo.
 echo Could not open url.
+goto fail
+
+:runcrud
+./runcrud.bat
+if %ERRORLEVEL% == "0" goto end
 goto fail
 
 :fail
@@ -19,6 +16,5 @@ echo.
 echo There were errors
 
 :end
-echo.
 echo Work is finished. Tasks are showed in the browser.
 
